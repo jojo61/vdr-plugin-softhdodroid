9f86e10fb619f505e1350f11e7d41409dedf23cc
jojo61 <git@jojo61.de>
2024-06-05 16:52:54 +0200

    Update Version

--- softhdodroid.cpp
+++ softhdodroid.cpp
@@ -59,7 +59,7 @@ extern "C"
 /// vdr-plugin version number.
 /// Makefile extracts the version number for generating the file name
 /// for the distribution archive.
-static const char *const VERSION = "4.11"
+static const char *const VERSION = "4.12"
 #ifdef GIT_REV
     "-GIT-" GIT_REV
 #endif
