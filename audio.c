ccc070cd885d93b2aab227e171ba5383bbca4ff3
jojo61 <git@jojo61.de>
2024-08-12 16:17:47 +0200

    Snal fix for previous commit

--- audio.c
+++ audio.c
@@ -2171,7 +2171,7 @@ uint64_t AudioGetClock(void)
             return AudioRing[AudioRingRead].PTS + 0 * 90 - delay;
         }
     }
-    return -1;
+    return 0;
 }
 uint64_t AudioGetwClock(void)
 {
